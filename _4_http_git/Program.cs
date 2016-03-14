29b344bc34b18071a755f348401eb1907c7a6454
DziLean <gestaltldi@yandex.ru>
2016-03-14 03:43:08 -0700

    Headers

--- _4_http_git/Program.cs
+++ _4_http_git/Program.cs
@@ -1,15 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net;
+using System.Threading;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _4_http_git
 {
-    class Program
+    public class WebServer
     {
-        static void Main(string[] args)
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+
+        public WebServer(string[] prefixes, Func<HttpListenerRequest, string> method)
         {
+            if (!HttpListener.IsSupported)
+                throw new NotSupportedException(
+                    "Needs Windows XP SP2, Server 2003 or later.");
+
+            // URI prefixes are required, for example 
+            // "http://localhost:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // A responder method is required
+            if (method == null)
+                throw new ArgumentException("method");
+
+            foreach (string s in prefixes)
+                _listener.Prefixes.Add(s);
+
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
+            : this(prefixes, method) { }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                string rstr = _responderMethod(ctx.Request);
+                                if(rstr != "H" && rstr != "P" && rstr !="G")
+                                    ctx.Response.Headers["Content-Type"] = "text/html";
+                                else
+                                     ctx.Response.Headers["Content-Type"] = "text/plain";
+                                Console.WriteLine("\r\n| Request Headers | ");
+                                for (int i = 0; i < ctx.Request.Headers.AllKeys.Length; ++i)
+                                    Console.WriteLine("+ " + ctx.Request.Headers.AllKeys[i] + ": " + ctx.Request.Headers[ctx.Request.Headers.AllKeys[i]]);
+                                Console.WriteLine("\r\n| Response Headers | ");
+                                ctx.Request.Headers["Host"] = ctx.Response.Headers["Host"];
+                                for (int i = 0; i < ctx.Response.Headers.AllKeys.Length; ++i)
+                                    Console.WriteLine("+ " + ctx.Response.Headers.AllKeys[i] + ": " + ctx.Response.Headers[ctx.Response.Headers.AllKeys[i]]);
+                                byte[] buf;
+                                if (rstr == "H")
+                                    buf = null;
+                                else
+                                    buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { } // suppress any exceptions
+                            finally
+                            {
+                                // always close the stream
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { } // suppress any exceptions
+            });
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
         }
     }
 }
--- _4_http_git/Run.cs
+++ _4_http_git/Run.cs
@@ -1,12 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net;
+using System.Threading;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace _4_http_git
 {
-    class Run
+    class Program
     {
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/test/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press any key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            string Response;
+            switch (request.Headers[0])
+            {
+                case "GET":
+                    Response = "G";
+                    break;
+                case "POST":
+                    Response = "P";
+                    break;
+                case "HEAD":
+                    Response = "H";
+                    break;
+                default:
+
+                    Response = "<!DOCTYPE html>" +
+                        "<style>input{background-color:blue;color:white;border-radius:5px;width:80px;height:40px;cursor:pointer;font-size:20px;font-family:serif;}</style>" +
+                            "</head><body>	" +
+                            "<input type=\"button\" id='get' value=\"GET\">	<input type=\"button\" id='post' value=\"POST\"><input type=\"button\" id='head' value=\"HEAD\">" +
+                            "<div id='response'>	RESPONSE</div>" +
+                            "<script>	get.onclick=function(){	var xhr = new XMLHttpRequest();\r\n	xhr.open('GET','/test',false);\r\n" +
+                            "xhr.onreadystatechange=function(){\r\n" +
+                            "if(xhr.readyState == 4 && xhr.status==200)" +
+                            "response.innerHTML = \"GET\";\r\n" +
+                            "};	xhr.setRequestHeader(\"Test\",\"GET\");\r\n" +
+                            "xhr.send();};\r\n	post.onclick=function(){	var xhr = new XMLHttpRequest();\r\n" +
+                            "xhr.open('POST','/test',false);\r\n" +
+                            "xhr.onreadystatechange=function(){" +
+                            "if(xhr.readyState == 4 && xhr.status==200)		" +
+                            "response.innerHTML = \"POST\";			};\r\n" +
+                            "xhr.setRequestHeader(\"Test\",\"POST\");\r\n" +
+                            "xhr.send();};head.onclick=function(){" +
+                            "var xhr = new XMLHttpRequest();\r\n" +
+                            "xhr.open('HEAD','/test',false);\r\n" +
+                            "xhr.onreadystatechange=function(){" +
+                            "if(xhr.readyState == 4 && xhr.status==200)" +
+                            "response.innerHTML = \"HEAD\";			};	xhr.setRequestHeader(\"Test\",\"HEAD\");	xhr.send();}</script></body>\r\n";
+                    break;
+            }
+            return Response;
+            //return "<!DOCTYPE html>"+
+            //    "<style>input{background-color:blue;color:white;border-radius:5px;width:80px;height:40px;cursor:pointer;font-size:20px;font-family:serif;}</style>" +
+            //        "</head><body>	" +
+            //        "<input type=\"button\" id='get' value=\"GET\">	<input type=\"button\" id='post' value=\"POST\"><input type=\"button\" id='head' value=\"HEAD\">" +
+            //        "<div id='response'>	RESPONSE</div>" +
+            //        "<script>get.onclick=function(){" +
+            //        "var xhr = new XMLHttpRequest();" +
+            //        "xhr.open('GET','/test',false);" +
+            //        "xhr.onreadystage=function(){" +
+            //        "if(xhr.readyState = 4 && xhr.status==200)response.innerHtml = \"GET\";};" +
+            //        "xhr.send();}</script></body>";
+        }
     }
 }
